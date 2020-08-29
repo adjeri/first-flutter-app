39036c53d95834b5ec79c791a6f7a3865d88fd9d
Adjéri SABI <kod047@gmail.com>
2020-08-29 22:26:42 +0900

    wisdom quotes

--- lib/main.dart
+++ lib/main.dart
@@ -7,5 +7,5 @@ import 'ui/home.dart';
 // }
 
 void main() => runApp(new MaterialApp(
-  home: BizCard(),
+  home: Wisdom(),
 ));
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,13 +1,91 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+class Wisdom extends StatefulWidget {
+  @override
+  _WisdomState createState() => _WisdomState();
+}
+
+class _WisdomState extends State<Wisdom> {
+  int _index = 0;
+  List quotes = [
+    "No one can make you feel inferior without your consent",
+    "It is better to remain silent at the risk of being thought a fool, than to talk and remove all doubt of it.",
+    "The fool doth think he is wise, but the wise man knows himself to be a fool.",
+    "Whenever you find yourself on the side of the majority, it is time to reform (or pause and reflect).",
+    "When someone loves you, the way they talk about you is different. You feel safe and comfortable",
+    "Knowing yourself is the beginning of all wisdom.",
+    "The only true wisdom is in knowing you know nothing.",
+    "The saddest aspect of life right now is that science gathers knowledge faster than society gathers wisdom.",
+    "Count your age by friends, not years. Count your life by smiles, not tears.",
+    "In a good bookroom you feel in some mysterious way that you are absorbing the wisdom contained in all the books through your skin, without even opening them.",
+    "Hold fast to dreams, For if dreams die, Life is a broken-winged bird, That cannot fly.",
+    "May you live every day of your life.",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: Container(
+                  width: 350,
+                    height: 200,
+                    margin: EdgeInsets.all(30.0),
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(14.5)
+                    ),
+                    child: Center(child: Text(quotes[_index % quotes.length],
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontStyle: FontStyle.italic,
+                        fontSize: 16.5,
+                      ),
+                    ))
+                ),
+              ),
+            ),
+            Divider(thickness: 1.3,),
+            Padding(
+              padding: const EdgeInsets.only(top:18.0),
+              child: FlatButton.icon(
+                  onPressed: _showQuote,
+                  color: Colors.greenAccent.shade700,
+                  icon: Icon(Icons.wb_sunny),
+                  label: Text("Inspire me!",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  )
+              ),
+            ),
+            Spacer(
+
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _showQuote() {
+    setState(() {
+      _index++;
+    });
+  }
+}
+
 class BizCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("BizCard")
-      ),
+      appBar: AppBar(title: Text("BizCard")),
       body: Container(
         alignment: Alignment.center,
         child: Stack(
@@ -27,13 +105,12 @@ class BizCard extends StatelessWidget {
       height: 200,
       margin: EdgeInsets.all(50),
       decoration: BoxDecoration(
-        color: Colors.green,
-        borderRadius: BorderRadius.circular(14.5)
-      ),
+          color: Colors.green, borderRadius: BorderRadius.circular(14.5)),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text("Adjeri Kodjiya SABI",
+          Text(
+            "Adjeri Kodjiya SABI",
             style: TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
@@ -43,10 +120,7 @@ class BizCard extends StatelessWidget {
           Text("Flutter fan"),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Icon(Icons.person_outline),
-              Text("@adjeri")
-            ],
+            children: <Widget>[Icon(Icons.person_outline), Text("@adjeri")],
           )
         ],
       ),
@@ -58,20 +132,18 @@ class BizCard extends StatelessWidget {
       width: 100,
       height: 100,
       decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(50.0)),
-        border: Border.all(
-          color: Colors.greenAccent,
-          width: 1.2,
-        ),
-        image: DecorationImage(image: NetworkImage("https://picsum.photos/300"),
-          fit: BoxFit.cover
-        )
-      ),
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(50.0)),
+          border: Border.all(
+            color: Colors.greenAccent,
+            width: 1.2,
+          ),
+          image: DecorationImage(
+              image: NetworkImage("https://picsum.photos/300"),
+              fit: BoxFit.cover)),
     );
   }
 }
-
 
 class ScaffoldExample extends StatelessWidget {
   _tapButton() {
@@ -101,11 +173,14 @@ class ScaffoldExample extends StatelessWidget {
         onPressed: () => debugPrint("button float"),
       ),
       bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.add_circle), title:Text("First")),
-            BottomNavigationBarItem(icon: Icon(Icons.ac_unit),title:Text("Second")),
-            BottomNavigationBarItem(icon: Icon(Icons.description),title:Text("Third"))
-          ],
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle), title: Text("First")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.ac_unit), title: Text("Second")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.description), title: Text("Third"))
+        ],
         onTap: (int index) => debugPrint("Tapped item : $index"),
       ),
       body: Container(
