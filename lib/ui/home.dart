34f85b913c981bf10443c64515fdb686a718822b
Adjéri SABI <kod047@gmail.com>
2020-08-29 21:01:34 +0900

    bizcard added

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,6 +1,78 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+class BizCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BizCard")
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: <Widget>[
+            _getCard(),
+            _getAvatar(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _getCard() {
+    return Container(
+      width: 350,
+      height: 200,
+      margin: EdgeInsets.all(50),
+      decoration: BoxDecoration(
+        color: Colors.green,
+        borderRadius: BorderRadius.circular(14.5)
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("Adjeri Kodjiya SABI",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          Text("Flutter fan"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.person_outline),
+              Text("@adjeri")
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Container _getAvatar() {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(50.0)),
+        border: Border.all(
+          color: Colors.greenAccent,
+          width: 1.2,
+        ),
+        image: DecorationImage(image: NetworkImage("https://picsum.photos/300"),
+          fit: BoxFit.cover
+        )
+      ),
+    );
+  }
+}
+
+
 class ScaffoldExample extends StatelessWidget {
   _tapButton() {
     debugPrint("tapped button");
